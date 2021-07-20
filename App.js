e2481d77e18b48d587b803de255360dd47d34ab6
guumee <GuuMee>
2021-07-20 15:49:11 +0300

    Added fonts

--- App.js
+++ App.js
@@ -1,33 +1,51 @@
 import { StatusBar as ExpoStatusBar } from "expo-status-bar";
 import React from "react";
-import { StyleSheet, Text, View, SafeAreaView, StatusBar } from "react-native";
+import { ThemeProvider } from "styled-components/native";
+import { theme } from "./src/infrastructure/theme";
+import { BookSearchScreen } from "./src/features/books/screens/search-menu/book-search.screen";
+import AppLoading from "expo-app-loading";
+
+import {
+  useFonts as useIndieFlower,
+  IndieFlower_400Regular,
+} from "@expo-google-fonts/indie-flower";
+import {
+  useFonts as usePangolin,
+  Pangolin_400Regular,
+} from "@expo-google-fonts/pangolin";
+import {
+  useFonts as useOswald,
+  Oswald_400Regular,
+} from "@expo-google-fonts/oswald";
+import { useFonts as useLato, Lato_400Regular } from "@expo-google-fonts/lato";
 
 export default function App() {
+  let [indieFlowerLoaded] = useIndieFlower({
+    IndieFlower_400Regular,
+  });
+
+  let [PangolinLoaded] = usePangolin({
+    Pangolin_400Regular,
+  });
+
+  let [OswaldLoaded] = useOswald({
+    Oswald_400Regular,
+  });
+
+  let [latoLoaded] = useLato({
+    Lato_400Regular,
+  });
+
+  if (!indieFlowerLoaded || !latoLoaded || !OswaldLoaded || !PangolinLoaded) {
+    return <AppLoading />;
+  }
+
   return (
     <>
-      <SafeAreaView style={styles.safeAreaStyle}>
-        <View style={styles.greenContainer}>
-          <Text>Heeeeey</Text>
-        </View>
-      </SafeAreaView>
+      <ThemeProvider theme={theme}>
+        <BookSearchScreen />
+      </ThemeProvider>
       <ExpoStatusBar style="auto" />
     </>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-  safeAreaStyle: {
-    flex: 1,
-    marginTop: StatusBar.currentHeight,
-  },
-  greenContainer: {
-    padding: 16,
-    backgroundColor: "green",
-  },
-});
